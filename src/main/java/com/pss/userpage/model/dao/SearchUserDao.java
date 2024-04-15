82b34326e1b6d7b0ec30c6c8d82bc093a73aa8e8
LumpenKoko <bornin1102@gmail.com>
2024-04-15 17:40:45 +0900

    userpage 관련 mapper-xml 수정, mybatis-config 수정

--- src/main/java/com/pss/userpage/model/dao/SearchUserDao.java
+++ src/main/java/com/pss/userpage/model/dao/SearchUserDao.java
@@ -8,9 +8,10 @@ public class SearchUserDao {
 	
 	public static ArrayList searchUser(SqlSession sqlSession, String nickname) {
 		ArrayList<Object> list = new ArrayList<>();
-		list.add(sqlSession.selectOne("userpage-mapper.SearchUser", nickname));
-		list.add(sqlSession.selectOne("userpage-mapper.SearchUserDiet", nickname));
-		list.add(sqlSession.selectOne("userpage-mapper.SearchUserExercise", nickname));
+		list.add(sqlSession.selectOne("userpage-mapper.SearchUserInfo", nickname));
+		list.add(sqlSession.selectOne("userpage-mapper.SearchUserPhysicalInfo", nickname));
+		list.add(sqlSession.selectOne("userpage-mapper.SearchUserDietRecord", nickname));
+		list.add(sqlSession.selectOne("userpage-mapper.SearchUserExerciseRecord", nickname));
 		return list;
 	}
 

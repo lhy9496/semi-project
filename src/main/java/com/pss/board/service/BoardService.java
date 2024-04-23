f0e84b65cefffdd1a5a5da1bdf148537ff4e0b25
88fury <jjaman3@naver.com>
2024-04-23 12:44:50 +0900

    보드 수정중

--- src/main/java/com/pss/board/service/BoardService.java
+++ src/main/java/com/pss/board/service/BoardService.java
@@ -4,17 +4,20 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import com.pss.board.model.vo.Board;
+import com.pss.board.model.vo.Reply;
 import com.pss.common.vo.PageInfo;
 
 public interface BoardService {
 
 	//게시판리스트 조회
-	public int selectListCount();
-	public ArrayList<Board> selectList(PageInfo pi);
-	
-	//게시글 검색
-	int selectSearchCount(HashMap<String, String> map);
-	ArrayList<Board> selectSearchList(HashMap<String, String> map, PageInfo pi);
-	
-	Board increaseCount(int boardNo);
+		public int selectListCount();
+		public ArrayList<Board> selectList(PageInfo pi);
+		
+		//게시글 검색
+		public int selectSearchCount(HashMap<String, String> map);
+		public ArrayList<Board> selectSearchList(HashMap<String, String> map, PageInfo pi);
+
+		//게시글 상세조회
+		public Board increaseCount(int boardNo);
+		public ArrayList<Reply> selectReplyList(int boardNo);
 }

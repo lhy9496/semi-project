b96011aae7fa7aaa369c491d3fb785c8409d4875
88fury <jjaman3@naver.com>
2024-04-24 18:38:23 +0900

    보드 데이터베이스 연결 완료 css 수정작업 해야함

--- src/main/java/com/pss/board/controller/BoardInsertController.java
+++ src/main/java/com/pss/board/controller/BoardInsertController.java
@@ -1,9 +1,7 @@
 package com.pss.board.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
-import javax.security.auth.message.callback.PrivateKeyCallback.Request;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.pss.board.model.vo.Board;
-import com.pss.board.model.vo.Reply;
 import com.pss.board.service.BoardService;
 import com.pss.board.service.BoardServiceImpl;
 import com.pss.member.model.vo.Member;
@@ -19,14 +16,14 @@ import com.pss.member.model.vo.Member;
 /**
  * Servlet implementation class BoardDetailController
  */
-@WebServlet("/rinsert.bo")
-public class BoardInsertReply extends HttpServlet {
+@WebServlet("/insert.bo")
+public class BoardInsertController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public BoardInsertReply() {
+    public BoardInsertController() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -35,24 +32,36 @@ public class BoardInsertReply extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	request.setCharacterEncoding("UTF-8");
-    	
-    	int boardNo = Integer.parseInt(request.getParameter("bno"));
-    	String replyContent = request.getParameter("content");
-    	int userNo = ((Member)request.getSession().getAttribute("loginUser")).getUserNo();    
-    	
-    	Reply r = new Reply();
-    	r.setRefBoardNo(boardNo);
-    	r.setReplyContent(replyContent);
-    	r.setReplyWriter(String.valueOf(userNo));
-    	
-    	BoardService bService = new BoardServiceImpl();
-    	
-    	int result = bService.insertReply(r);
-    	
-    	response.getWriter().print(result);
-    	
-    	
+		request.setCharacterEncoding("UTF-8");
+		
+		int userNo = ((Member)(request.getSession().getAttribute("loginUser"))).getUserNo();
+		
+		int category = Integer.parseInt(request.getParameter("category"));
+		String title = request.getParameter("title");
+		String content = request.getParameter("content");
+		
+		
+		
+		
+		Board b = new Board();
+		
+		b.setBoardTitle(title);
+		b.setBoardContent(content);
+		b.setCategoryNo(category);
+		b.setBoardWriter(String.valueOf(userNo));
+		
+		BoardService bService = new BoardServiceImpl();
+		
+		int result = bService.insertBoard(b);
+			
+		if(result > 0) {
+			request.getSession().setAttribute("alertMsg", "일반게시글 작성 성공");
+			response.sendRedirect(request.getContextPath() + "/list.bo?cpage=1");
+		}else {
+			request.setAttribute("errorMsg", "일반게시글 작성 실패");
+			request.getRequestDispatcher("views/board/boardListView.jsp").forward(request, response);
+		}
+		
 	}
 
 	/**
